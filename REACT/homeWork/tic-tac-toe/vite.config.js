55834476b1ce568ed7590f2b8d2051202b1558e5
ivanandreevich447 <bos447@mail.ru>
2025-04-06 13:24:02 +0300

    2

--- REACT/homeWork/tic-tac-toe/vite.config.js
+++ REACT/homeWork/tic-tac-toe/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //добавил ниже имя проекта
+  //добавил ниже имя проекта на англ
   base: '/tic-tac-toe'
 })
